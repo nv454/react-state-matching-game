28effbabe8cb8ce016d00fd617af7bdf10f9c1f8
NV <nikola.vrbinarova@accenture.com>
2021-09-13 15:17:56 +0200

    4 version

--- refactor/components/TileSelector/TileSelector.js
+++ refactor/components/TileSelector/TileSelector.js
@@ -8,33 +8,33 @@ const TileSelector = (props) => {
 const [ref, hovered] = useHover();
 
 
-return(
-    <GameContext.Consumer>
-{
-({numTiles, handleNumTileChange})=>{
+return( 
+  <GameContext.Consumer>
+    {
+      ({numTiles, handleNumTileChange})=>
+      {
+          const dropdown = hovered ? (
+                <div className='tileSelectorContent' >
+                  <div className='number' onClick ={handleNumTileChange(4)}>4</div>
+                  <div className='number' onClick ={handleNumTileChange(16)}>16</div>
+                  <div className='number' onClick ={handleNumTileChange(36)}>36</div>
+                </div>
+          ) :null;
 
-  const dropdown = hovered ? (
-        <div className='tileSelectorContent' >
-          <div className='number' onClick ={handleNumTileChange(4)}>4</div>
-          <div className='number' onClick ={handleNumTileChange(16)}>16</div>
-          <div className='number'onClick ={handleNumTileChange(36)}>36</div>
-        </div>
-  ) :null;
-
-  return (
-   <div className='tileSelector'>
-     <div>Number of Tiles</div>
-     <div className='tileSelectorDropdown' ref={ref}>
-       {numTiles}
-       {dropdown}
-       
-     </div>
-   </div>
- )
-}
-}
-</GameContext.Consumer>
-  )
+          return (
+          <div className='tileSelector'>
+            <div>Number of Tiles</div>
+            <div className='tileSelectorDropdown' ref={ref}>
+              {numTiles}
+              {dropdown}
+              
+            </div>
+          </div>
+        )
+      }
+    }
+  </GameContext.Consumer>
+)
 
 }
 
